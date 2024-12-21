cafb8754979d55f73f8b89e5e9757bc64e2c5efb
Ilia Bulkin <bulkin20088@gmail.com>
2024-12-21 19:03:33 +0200

    [18.12.2024]

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,44 +1,55 @@
 import { useParams } from "react-router-dom"
 import "./PostPage.css"
 import { useEffect, useState } from "react"
+import { usePosts } from "../../hooks/usePosts"
 
-export function PostPage(){
+export function PostPage() {
     const [post, setPost] = useState({
-        id: 0, 
-        title: "", 
-        cover_image: "", 
-        tags: [""], 
-        body_markdown:""
+        id: 0,
+        title: "",
+        cover_image: "",
+        tags: [""],
+        body_markdown: "",
     })
     const params = useParams()
-    
+    const [isLoading, setIsLoading] = useState(true)
+    const { posts } = usePosts()
+
     useEffect(() => {
-        async function getAllPosts(){
+        async function getAllPosts() {
+            setIsLoading(true)
             const response = await fetch(`https://dev.to/api/articles/${params.id}`)
             const post = await response.json()
             setPost(post)
+            setIsLoading(false)
         }
         getAllPosts()
     }, [params.id])
 
     return (
         <div id="PostPage">
-            <div id="titleOfPostDiv">
-                <h1 id="titleOfPost">title: {post.title}</h1>
-            </div>
-            <div id="imageOfPostDiv">
-                <img id="imageOfPost" src={post.cover_image} alt="imageOfPostError" />
-            </div>
-            <div id="tagsOfPost">
-                <h2 id="tagsTitle">tags:</h2>
-                {post.tags.map((tag) => {
-                    return <p>{tag}</p>
-                })}
-            </div>
-            <div id="bodyMarkdown">
-                <h2 id="bodyMarkdownTitle">body markdown:</h2>
-                <p id="bodyMarkdownText">{post.body_markdown}</p>
-            </div>
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : (
+                <div>
+                    <div id="titleOfPostDiv">
+                        <h1 id="titleOfPost">title: {post.title}</h1>
+                    </div>
+                    <div id="imageOfPostDiv">
+                        <img id="imageOfPost" src={post.cover_image} alt="imageOfPostError" />
+                    </div>
+                    <div id="tagsOfPost">
+                        <h2 id="tagsTitle">tags:</h2>
+                        {post.tags.map((tag, index) => {
+                            return <p key={index}>{tag}</p>;
+                        })}
+                    </div>
+                    <div id="bodyMarkdown">
+                        <h2 id="bodyMarkdownTitle">body markdown:</h2>
+                        <p id="bodyMarkdownText">{post.body_markdown}</p>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
