f87f5fc6452fa4ee612b597be633cf21b53c7e3a
IliaBulkin <bulkin20088@gmail.com>
2025-01-22 13:37:44 +0200

    [19.01.2025] non final homework^

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -3,6 +3,14 @@ import { Layout } from "./Layout/Layout"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { PostPage } from "../pages/PostPage/PostPage"
 import { useTitle } from "../hooks/useTitle"
+import { createContext } from "react"
+
+interface ILike{
+    like: boolean
+}
+
+const initialValue: ILike[] = []
+const likeContext = createContext< ILike[] >(initialValue)
 
 export function App(){
     useTitle('Абоба заголовок')
@@ -13,6 +21,7 @@ export function App(){
             <img src="https://m.media-amazon.com/images/I/51Dt2ljQJkS._AC_UF894,1000_QL80_.jpg" alt="no image f" />
 
             <h1>Список постов</h1>
+            <likeContext.Provider value={[{like: true}]}></likeContext.Provider>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Layout></Layout>}>
