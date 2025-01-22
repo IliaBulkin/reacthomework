ce5bb53f0e7aada56c78b583235f873219c94140
IliaBulkin <bulkin20088@gmail.com>
2025-01-22 15:02:27 +0200

    [19.01.2025] homework like and context, maybe it's working

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -1,35 +1,44 @@
-import { PostList } from "./PostList/PostList"
-import { Layout } from "./Layout/Layout"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { PostPage } from "../pages/PostPage/PostPage"
-import { useTitle } from "../hooks/useTitle"
-import { createContext } from "react"
+import { PostList } from "./PostList/PostList";
+import { Layout } from "./Layout/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PostPage } from "../pages/PostPage/PostPage";
+import { useTitle } from "../hooks/useTitle";
+import { createContext, useState } from "react";
 
-interface ILike{
-    like: boolean
+interface ILikeContext {
+    like: boolean;
+    toggleLike: () => void;
 }
 
-const initialValue: ILike[] = []
-const likeContext = createContext< ILike[] >(initialValue)
+const LikeContext = createContext<ILikeContext | null>(null);
 
-export function App(){
-    useTitle('Абоба заголовок')
+export function App() {
+    useTitle("Абоба заголовок");
+
+    const [like, setLike] = useState(false);
+
+    const toggleLike = () => {
+        setLike((prev) => !prev);
+    };
+
+
     return (
         <div id="8chan">
             <h1>Форум 8chan</h1>
             <p>Форум 8chan - анонимный форум, где вы можете делится своими переживаниями, помогать другим людям, писать книги</p>
-            <img src="https://m.media-amazon.com/images/I/51Dt2ljQJkS._AC_UF894,1000_QL80_.jpg" alt="no image f" />
+            <img src="https://m.media-amazon.com/images/I/51Dt2ljQJkS._AC_UF894,1000_QL80_.jpg" alt="no image f"/>
 
             <h1>Список постов</h1>
-            <likeContext.Provider value={[{like: true}]}></likeContext.Provider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout></Layout>}>
-                        <Route path="/PostList" element={<PostList></PostList>}></Route>
-                        <Route path="/PostList/:id" element={<PostPage></PostPage>}></Route>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+            <LikeContext.Provider value={{ like, toggleLike }}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout></Layout>}>
+                            <Route path="/PostList" element={<PostList></PostList>}></Route>
+                            <Route path="/PostList/:id" element={<PostPage></PostPage>}></Route>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </LikeContext.Provider>
         </div>
     );
-};  
+}
