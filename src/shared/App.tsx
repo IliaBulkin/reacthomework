6ac137f497d3afb7c2ba90ecd07d30cf25848d9a
Ilia Bulkin <bulkin20088@gmail.com>
2025-01-26 01:26:35 +0200

    [22.01.2025] like and context homework, not download all posts, save information about favorite

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -5,10 +5,17 @@ import { PostPage } from "../pages/PostPage/PostPage";
 import { useTitle } from "../hooks/useTitle";
 import { createContext, useState } from "react";
 
+export interface IPostProps {
+    id: number;
+    title: string;
+    content: string;
+    author: string;
+    date: string;
+}
+
 interface ILikeContext {
-    like: boolean;
-    likedPosts: number[]; // список лайкнутых постов
-    toggleLike: (postId?: number) => void; // функция переключения лайков
+    likedPosts: IPostProps[]; 
+    toggleLike: (post: IPostProps) => void; 
 }
 
 const LikeContext = createContext<ILikeContext | null>(null);
@@ -16,30 +23,30 @@ const LikeContext = createContext<ILikeContext | null>(null);
 export function App() {
     useTitle("Абоба заголовок");
 
-    const [like, setLike] = useState(false);
+    const [likedPosts, setLikedPosts] = useState<IPostProps[]>([]); 
 
-    const [likedPosts, setLikedPosts] = useState<number[]>([]);
-
-    const toggleLike = (postId?: number) => {
-        if (postId !== undefined) {
-            setLikedPosts((prev) =>
-                prev.includes(postId)
-                    ? prev.filter((id) => id !== postId) // удаляем из массива
-                    : [...prev, postId] // добавляем в массив
-            );
-        } else {
-            setLike((prev) => !prev);
-        }
+    const toggleLike = (post: IPostProps) => {
+        setLikedPosts((prev) => {
+            const isLiked = prev.some((likedPost) => likedPost.id === post.id); 
+            if (isLiked) {
+                return prev.filter((likedPost) => likedPost.id !== post.id); 
+            } else {
+                return [...prev, post];
+            }
+        });
     };
 
     return (
         <div id="8chan">
             <h1>Форум 8chan</h1>
             <p>Форум 8chan - анонимный форум, где вы можете делиться своими переживаниями, помогать другим людям, писать книги</p>
-            <img src="https://m.media-amazon.com/images/I/51Dt2ljQJkS._AC_UF894,1000_QL80_.jpg" alt="no image f"/>
+            <img
+                src="https://m.media-amazon.com/images/I/51Dt2ljQJkS._AC_UF894,1000_QL80_.jpg"
+                alt="no image"
+            />
 
             <h1>Список постов</h1>
-            <LikeContext.Provider value={{ like, likedPosts, toggleLike}}>
+            <LikeContext.Provider value={{ likedPosts, toggleLike }}>
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<Layout />}>
